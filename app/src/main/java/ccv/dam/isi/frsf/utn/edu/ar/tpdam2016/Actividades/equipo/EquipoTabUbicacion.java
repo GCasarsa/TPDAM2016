cee4bcb288e074cd09e66cf7d1eb2afb010b1f18
gcasarsa <gabrielcasarsa@gmail.com>
2017-02-02 12:24:42 -0300

    Agregada la distancia a la cancha

--- app/src/main/java/ccv/dam/isi/frsf/utn/edu/ar/tpdam2016/Actividades/equipo/EquipoTabUbicacion.java
+++ app/src/main/java/ccv/dam/isi/frsf/utn/edu/ar/tpdam2016/Actividades/equipo/EquipoTabUbicacion.java
@@ -1,6 +1,10 @@
 package ccv.dam.isi.frsf.utn.edu.ar.tpdam2016.Actividades.equipo;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +18,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import ccv.dam.isi.frsf.utn.edu.ar.tpdam2016.Entidades.Equipo;
@@ -26,13 +31,13 @@ public class EquipoTabUbicacion  extends Fragment implements OnMapReadyCallback 
     private GoogleMap mapa;
     private static Equipo equipo;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.activity_equipo_tab_ubicacion, container, false);
         ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
         return rootView;
     }
-
 
     public static EquipoTabUbicacion newInstance(int pagina, String titulo, Equipo equipoRecibido) {
         EquipoTabUbicacion fragmento = new EquipoTabUbicacion();
@@ -45,17 +50,25 @@ public class EquipoTabUbicacion  extends Fragment implements OnMapReadyCallback 
     }
 
     @Override public void onMapReady(GoogleMap googleMap){
+        if(ActivityCompat.checkSelfPermission(getActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},9999);
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
         mapa= googleMap;
-        mapa.addMarker(new MarkerOptions().position(new LatLng(equipo.getLatitud(), equipo.getLongitud())).title(equipo.getDescripcionEstadio()));
+        mapa.setMyLocationEnabled(true);
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        float[] resultado = new float[1];
+        MarkerOptions marcador = new MarkerOptions().position(new LatLng(equipo.getLatitud(), equipo.getLongitud())).title(equipo.getDescripcionEstadio());
+        android.location.Location.distanceBetween(marcador.getPosition().latitude, marcador.getPosition().longitude, location.getLatitude(), location.getLongitude(), resultado);
+        marcador.title(equipo.getDescripcionEstadio());
+        marcador.snippet("Distancia: " + Math.round(resultado[0]/1000)  +"Km");
+        mapa.addMarker(marcador);
         mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(equipo.getLatitud(), equipo.getLongitud()),16));
         mapa.getUiSettings().setZoomControlsEnabled(true);
         mapa.getUiSettings().setCompassEnabled(true);
-        if(ActivityCompat.checkSelfPermission(getActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},9999);
-            mapa.setMyLocationEnabled(true);
-            return;
-        }
-        mapa.setMyLocationEnabled(true);
     }
 
 }
